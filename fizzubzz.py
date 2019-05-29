eb3ba0e3b6ec417c264690e92fe9557497a3b67e
CharmotSebastian <SebastianCharmot@CharmotSebastians-MacBook-Pro.local>
2019-05-29 13:23:56 -0400

    fizzubzz.py

--- fizzubzz.py
+++ fizzubzz.py
@@ -2,17 +2,15 @@
 
 x = 0
 
-num = 19
-
-#def input(num):
-for i in range(1,20):
-    if i%3 == 0 and i%5 == 0:
-        print('FizzBuzz')
-    elif i%3 == 0: 
-        print('Fizz')
-    elif i%5 == 0:
-        print('Buzz') 
-    else:   
-        print(i)
+def input(num):
+    for i in range(1,num+1):
+        if i%3 == 0 and i%5 == 0:
+            print('FizzBuzz')
+        elif i%3 == 0: 
+            print('Fizz')
+        elif i%5 == 0:
+            print('Buzz') 
+        else:   
+            print(i)
 
 
